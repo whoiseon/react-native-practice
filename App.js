512602a16c8cabfaff29d604cc6671ae48e455a6
whoiseon <whois__@naver.com>
2023-04-08 18:02:16 +0900

    feat: get my city name by expo-location

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import getCurrentDate from "./lib/getCurrentDate";
+import CurrentWeather from "./components/CurrentWeather";
+import WeatherList from "./components/WeatherList";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, RN App!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.main}>
+        <CurrentWeather />
+        <WeatherList />
+        <View style={styles.testView}></View>
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +22,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#131313",
+  },
+  main: {
+    flex: 1,
+    marginTop: -16,
+  },
+  testView: {
+    flex: 2,
   },
 });
